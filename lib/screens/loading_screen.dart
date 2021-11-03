b65dc7f987a9d83431309b8740d629fd8c0cfe1e
jvsena42 <jvsena16@gmail.com>
2021-11-03 07:44:47 -0300

    feat: get method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,8 +21,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
     location.getCurrentLocation();
   }
 
+  Future<void> getData() async {
+    var uri = Uri.https('samples.openweathermap.org', 'data/2.5/weather',{
+      'lat': '39',
+      'lon': '139',
+      'appid': 'b6907d289e10d714a6e88b30761fae22'
+    });
+    var response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      print(jsonResponse);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold( );
   }
 }
