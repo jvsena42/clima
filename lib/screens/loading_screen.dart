ec6e844138b8e6f3a1e788cd201a49b612bb8978
jvsena42 <jvsena16@gmail.com>
2021-11-04 07:42:44 -0300

    feat: networking.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,9 @@
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
+
+
+const apiKey = 'b6907d289e10d714a6e88b30761fae22';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,34 +12,33 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  double? latitude;
+  double? longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
-    location.getCurrentLocation();
-  }
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-  Future<void> getData() async {
-    var uri = Uri.https('samples.openweathermap.org', 'data/2.5/weather',{
-      'lat': '39',
-      'lon': '139',
-      'appid': 'b6907d289e10d714a6e88b30761fae22'
+    var uri = Uri.https('api.openweathermap.org', 'data/2.5/weather',{
+      'lat': '$latitude',
+      'lon': '$longitude',
+      'appid': apiKey
     });
-    var response = await http.get(uri);
 
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      print(jsonResponse);
-    }
+    var networkHelper = NetworkHelper(uri);
+    print(networkHelper.getData());
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold( );
   }
 }
---
+++ lib/services/networking.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class NetworkHelper {
+
+  final Uri url;
+
+  NetworkHelper(this.url);
+
+  Future<void> getData() async {
+
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      return jsonResponse;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
