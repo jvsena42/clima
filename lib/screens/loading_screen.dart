0d8ba33588375a68ab525f9bea754f121f42d19f
jvsena42 <jvsena16@gmail.com>
2021-11-05 10:32:13 -0300

    refact: refactor loading sreen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,7 @@
 import 'package:clima/screens/location_screen.dart';
-import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = 'c1adbc9bb497fc51ff317e7be2f7bd58';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,14 +17,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
 
-    var uri = Uri.https('api.openweathermap.org', 'data/2.5/weather',
-        {'lat': '${location.latitude}', 'lon': '${location.longitude}','units':'metric', 'appid': apiKey});
-
-    var networkHelper = NetworkHelper(uri);
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(
       context,
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,23 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = 'c1adbc9bb497fc51ff317e7be2f7bd58';
+
 class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    var uri = Uri.https('api.openweathermap.org', 'data/2.5/weather',
+        {'lat': '${location.latitude}', 'lon': '${location.longitude}','units':'metric', 'appid': apiKey});
+
+    var networkHelper = NetworkHelper(uri);
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
